510a6dfb48c6a3a78e39c70f87880922e0649112
ViudiraTech <2609948707@qq.com>
2024-10-06 22:41:55 +0800

    更新了蜂鸣器驱动

---
+++ drivers/sound/beep.cpp
@@ -0,0 +1,35 @@
+/*
+ *
+ *		beep.cpp
+ *		板载蜂鸣器驱动
+ *
+ *		2024/10/6 By MicroFish
+ *		基于 GPL-3.0 开源协议
+ *		Copyright © 2020 ViudiraTech，保留所有权利。
+ *
+ */
+
+#include "beep.h"
+#include "common.h"
+
+/* 设置板载蜂鸣器状态 */
+VOID
+system_beep(
+	int hertz
+	)
+{
+	int i;
+	if (hertz == 0) {
+		/* 如果音调（Hz）为0，则关闭板载蜂鸣器 */
+		i = inb(0x61);					// 读取当前的板载蜂鸣器状态
+		outb(0x61, i & 0x0d);			// 关闭板载蜂鸣器
+	} else {
+		/* 设置板载蜂鸣器 */
+		i = hertz;
+		outb(0x43, 0xb6);				// 发送命令以设置计时器2
+		outb(0x42, i & 0xff);			// 发送分频值的低字节
+		outb(0x42, i >> 8);				// 发送分频值的高字节
+		i = inb(0x61);					// 读取当前板载蜂鸣器状态
+		outb(0x61, (i | 0x03) & 0x0f);	// 打开板载蜂鸣器
+	}
+}
